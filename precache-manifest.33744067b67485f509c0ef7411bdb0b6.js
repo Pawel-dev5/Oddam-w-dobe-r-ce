1c2312c7d740eb5f0a659ae9d2a5495f52d19ae3
Paweł <p.nowecki@gmail.com>
2020-10-02 15:25:45 +0200

    Updates

---
+++ precache-manifest.33744067b67485f509c0ef7411bdb0b6.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d8cc0dd41ad65b0af635acd616ee45",
+    "url": "/oddamwdoberece/index.html"
+  },
+  {
+    "revision": "56d90272ab9ffffad0cc",
+    "url": "/oddamwdoberece/static/css/main.5f9e7b60.chunk.css"
+  },
+  {
+    "revision": "147a9deaa9cf2cac9ea1",
+    "url": "/oddamwdoberece/static/js/2.60e33136.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/oddamwdoberece/static/js/2.60e33136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56d90272ab9ffffad0cc",
+    "url": "/oddamwdoberece/static/js/main.31439c58.chunk.js"
+  },
+  {
+    "revision": "60c4b4bccafaf14bb33a",
+    "url": "/oddamwdoberece/static/js/runtime-main.e4b1f7ac.js"
+  },
+  {
+    "revision": "99f2e620a9f4302f9861282162231969",
+    "url": "/oddamwdoberece/static/media/3 Columns Background.99f2e620.png"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/oddamwdoberece/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "aab5aa4cf97893341cd5aed84c736cd5",
+    "url": "/oddamwdoberece/static/media/Background-Form.aab5aa4c.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/oddamwdoberece/static/media/Decoration.2290784e.svg"
+  },
+  {
+    "revision": "df65b6c0ffa42176de9927a6a55fb508",
+    "url": "/oddamwdoberece/static/media/Facebook.df65b6c0.svg"
+  },
+  {
+    "revision": "87008d4ca9bf5d7f61c34875b5ff53ad",
+    "url": "/oddamwdoberece/static/media/Header-Form-Background.87008d4c.png"
+  },
+  {
+    "revision": "e99903ed42896ab89edf6c2e1d2add9e",
+    "url": "/oddamwdoberece/static/media/Home-Hero-Image.e99903ed.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/oddamwdoberece/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/oddamwdoberece/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/oddamwdoberece/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/oddamwdoberece/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "703479fdb58a909c7d696f41dc955649",
+    "url": "/oddamwdoberece/static/media/Instagram.703479fd.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/oddamwdoberece/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/oddamwdoberece/static/media/Signature.e54cfcfc.svg"
+  }
+]);
