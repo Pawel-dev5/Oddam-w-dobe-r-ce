3b0c365749c847aa900b22a9c190dbe2a05959a4
Paweł <p.nowecki@gmail.com>
2020-10-13 16:53:25 +0200

    Zrobione zkladanie konta firebase cos nie tak z logowaniem

--- src/App.js
+++ src/App.js
@@ -1,39 +1,153 @@
-import React from "react";
-import {
-  BrowserRouter,
-  Route,
-  Switch,
-} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import fire from "./components/fire";
+// import {
+//   BrowserRouter,
+//   Route,
+//   Switch,
+// } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import LogOut from "./components/LogOut";
 import CreateAccount from "./components/CreateAccount";
 import OddajRzeczy from "./components/OddajRzeczyMain";
 import NotFound from "./components/NotFound";
+import NavLogin from "./components/NavLogin";
+import Nav from "./components/Nav";
+import Hero from "./components/Hero";
 
-function App(props) {
+
+function App() {
   // const isLogged = props.user;
   // console.log(isLogged.state);
-  console.log(props.user)
+  // console.log(props.user)
+
+  const [user, setUser] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [hasAccount, setHasAccount] = useState(false);
+
+  const clearInputs = () => {
+    setEmail('');
+    setPassword('');
+  }
+
+  const clearErrors = () => {
+    setEmailError('');
+    setPasswordError('');
+  }
+
+  const handleLogin = () => {
+    clearErrors();
+    fire
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(err => {
+        switch (err.code) {
+          case "auth/invalid-email":
+          case "auth/user-disabled":
+          case "auth/user-not-found":
+            setEmailError(err.message);
+            break;
+          case "auth/wrong-password":
+            setPasswordError(err.message);
+            break;
+        }
+      });
+  };
+
+  const handleSignup = () => {
+    clearErrors();
+    fire
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch(err => {
+        switch (err.code) {
+          case "auth/email-already-in-use":
+          case "auth/invalid-email":
+            setEmailError(err.message);
+            break;
+          case "auth/weak-password":
+            setPasswordError(err.message);
+            break;
+        }
+      });
+  };
+
+  const handleLogout = () => {
+    fire.auth().signOut();
+  };
+
+  const authListener = () => {
+    fire.auth().onAuthStateChanged((user) => {
+      if (user) {
+        clearInputs();
+        setUser(user);
+      } else {
+        setUser("");
+      }
+    });
+  };
+
+  useEffect(() => {
+    authListener();
+  }, [])
+
   return (
-    <BrowserRouter>
-      <Switch>
-        {/* <Route path="/"> 
+    <>
+      {/* <BrowserRouter>
+      <Switch> */}
+      {/* {user ? <NavLogin /> : <Nav />}  */}
+      {/* <Route path="/"> 
             <Home user={user} ></Home>
           </Route> */}
-        <Route exact path="/" component={Home} />
-        {/* <Route path={LINKS_CONFIG.Onas} component={About} /> */}
-        {/* <Route path="/Ocochodzi/:carId" component={CarDetails} /> */}
-        <Route path="/logowanie" component={Login} />
-        <Route path="/oddaj-rzeczy" component={OddajRzeczy} />
-        {/* <Route path="/oddaj-rzeczy">
+      {/* <Route exact path="/" component={Home} /> */}
+      {/* <Route path={LINKS_CONFIG.Onas} component={About} /> */}
+      {/* <Route path="/Ocochodzi/:carId" component={CarDetails} /> */}
+      {/* <Route path="/logowanie" 
+        component={Login} 
+        email={email} 
+        setEmail={setEmail} 
+        password={setPassword} 
+        setPassword={setPassword} 
+        handleLogin={handleLogin} 
+        handleSignup={handleSignup} 
+        hasAccount={hasAccount}
+        setHasAccount={setHasAccount}
+        emailError={emailError}
+        passwordError={passwordError} */}
+      {/* /> */}
+      {/* <Route path="/oddaj-rzeczy" component={OddajRzeczy} /> */}
+      {/* <Route path="/oddaj-rzeczy">
             {!props.user ? <Login /> : <OddajRzeczy/>}
           </Route> */}
-        <Route path="/rejestracja" component={CreateAccount} />
+      {/* <Route path="/rejestracja" component={CreateAccount} />
         <Route path="/wylogowano" component={LogOut} />
         <Route component={NotFound} />
       </Switch>
-    </BrowserRouter>
+    </BrowserRouter> */}
+    <div>
+      {user ? (
+        <Hero handleLogout={handleLogout} />
+      ) : (
+          <Login
+            component={Login}
+            email={email}
+            setEmail={setEmail}
+            password={password}
+            setPassword={setPassword}
+            handleLogin={handleLogin}
+            handleSignup={handleSignup}
+            hasAccount={hasAccount}
+            setHasAccount={setHasAccount}
+            emailError={emailError}
+            passwordError={passwordError}
+          />
+        )}
+      {/* <Hero handleLogout={handleLogout} /> */}
+    </div>
+    </>
   );
 }
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -5,91 +5,132 @@ import NavLogin from "./NavLogin";
 import Nav from "./Nav";
 import UsersAPI from "./Users.js";
 
-const Login = () => {
-  const [login, setLogin] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const [errorLogin, setErrorLogin] = useState("");
-  const [errorPassword, setErrorPassword] = useState("");
-  const [user, setUser] = useState(false);
+const Login = (props) => {
+  // const [login, setLogin] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [error, setError] = useState("");
+  // const [errorLogin, setErrorLogin] = useState("");
+  // const [errorPassword, setErrorPassword] = useState("");
+  // const [user, setUser] = useState(false);
 
-  console.log(user.login)
-  const submit = e => {
-    e.preventDefault();
-    const isErrorLogin = login.length < 5;
-    const isErrorPassword = password.length < 5;
+  // console.log(user.login)
+  // const submit = e => {
+  //   e.preventDefault();
+  //   const isErrorLogin = login.length < 5;
+  //   const isErrorPassword = password.length < 5;
 
-    if (isErrorLogin) {
-      setErrorLogin("Login nieprawidłowy");
-      return;
-    } else {
-      setErrorLogin("")
-    }
-    if (isErrorPassword) {
-      setErrorPassword("Hasło nieprawidłowe");
-      return;
-    } else {
-      setErrorPassword("")
-    }
+  //   if (isErrorLogin) {
+  //     setErrorLogin("Login nieprawidłowy");
+  //     return;
+  //   } else {
+  //     setErrorLogin("")
+  //   }
+  //   if (isErrorPassword) {
+  //     setErrorPassword("Hasło nieprawidłowe");
+  //     return;
+  //   } else {
+  //     setErrorPassword("")
+  //   }
 
-    UsersAPI.login({
-      username: login,
-      password: password
-    })
-      .then(user => {
-        setUser(user);
-      })
-      .catch(err => {
-        setError(err);
-      });
-  };
+  //   UsersAPI.login({
+  //     username: login,
+  //     password: password
+  //   })
+  //     .then(user => {
+  //       setUser(user);
+  //     })
+  //     .catch(err => {
+  //       setError(err);
+  //     });
+  // };
+
+  const {
+    email,
+    setEmail,
+    password,
+    setPassword,
+    handleLogin,
+    handleSignup,
+    hasAccount,
+    setHasAccount,
+    emailError,
+    passwordError,
+  } = props;
 
   return (
     <>
-      {user ? <NavLogin /> : <Nav />}
-      <div id="LoginContainer">
+      {/* {user ? <NavLogin /> : <Nav />} */}
+      {/* <Nav/> */}
+      <div id="LoginContainer" className="login">
         <div id="LoginContainerHeader">
           <h1>Zaloguj się</h1>
           <img id="decoration" src={Decoration} alt="decoration"></img>
         </div>
         <div id="LoginForm">
 
-          <form onSubmit={submit}>
+          <form >
             <div className="LoginFormContainer">
               <label>
                 <p>Email</p>
                 <input
                   type="text"
-                  value={login}
-                  onChange={e => setLogin(e.target.value)}
+                  autoFocus
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                // value={login}
+                // onChange={e => setLogin(e.target.value)}
                 />
-                {errorLogin && <h1>{errorLogin}</h1>}
+                <p className="errorMsg">{emailError}</p>
+                {/* {errorLogin && <h1>{errorLogin}</h1>} */}
               </label>
               <label>
                 <p>Hasło</p>
                 <input
                   type="password"
+                  autoFocus
+                  required
                   value={password}
-                  onChange={e => setPassword(e.target.value)}
-                />
-                {errorPassword && <h1>{errorPassword}</h1>}
-              </label>
+                  onChange={(e) => setPassword(e.target.value)}
+                  />
+                  </label>
+                <p className="errorMsg">{passwordError}</p>
+                {/* {errorPassword && <h1>{errorPassword}</h1>} */}
+              {/* </label> */}
             </div>
-            {error && <h1>{error}</h1>}
+            {/* {error && <h1>{error}</h1>} */}
             <div id="LoginButtons">
-              <Link to="rejestracja">
-                <button id="LoginButtons1">
+              {hasAccount ? (
+                <>
+                  <button id="LoginButtons1" onClick={handleLogin}>Zaloguj</button>
+                  <p>
+                    Nie masz konta?
+                   <span onClick={() => setHasAccount(!hasAccount)}>Utwórz konto</span>
+                  </p>
+                </>
+              ) : (
+                  <>
+                    <button id="LoginButtons1" onClick={handleSignup}>Załóż konto</button>
+                    <p>
+                      Masz konto?
+                      <span onClick={() => setHasAccount(!hasAccount)}>Zaloguj</span>
+                    </p>
+                  </>
+                )}
+              {/* <Link to="rejestracja"> */}
+                {/* <button id="LoginButtons1">
                   Załóż konto
-                  </button>
-              </Link>
-              <button
-                id="LoginButtons2"
-                type="submit"
+                  </button> */}
+              {/* </Link> */}
+              {/* <button
+              id="LoginButtons2"
+              onClick={handleLogin}
+                // type="submit"
               // user={user}
               // onChange={user ? <NavLogin /> : <Nav />}
               // onChange={e => setUser(e.target.value)}
               // jak zrobić zmianę url
-              >Zaloguj się</button>
+              >Zaloguj się</button> */}
             </div>
           </form>
         </div>
